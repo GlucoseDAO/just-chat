15f68bfc66d759beee5dd2552613c63b1600b358
Livia_Zaharia <liviazaharia202@gmail.com>
2025-03-13 23:50:33 +0200

    adding fallback -two agents back to back

---
+++ test/test.py
@@ -0,0 +1,14 @@
+from just_agents.base_agent import BaseAgent
+import litellm
+
+# Enable debug mode to see detailed API call information
+litellm._turn_on_debug()
+
+if __name__ == "__main__":
+    llm_options = {
+        "model": "custom/sugar_genie_original",
+        "api_base": "http://localhost:8089" #stuff with /v1/completions and /v1 also does not work
+    }
+    agent = BaseAgent(llm_options=llm_options, system_prompt="You are a helpful assistant that can answer questions about Glucosedao and its founders.")
+    response = agent.query("What is Glucosedao and who are the founders?")
+    print(response)
